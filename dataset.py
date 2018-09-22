873dbec5975c30dbfe33ee30a0955aa7935bedc2
rsimpson <rsimpson@shatterit.com>
2018-09-22 16:35:41 -0400

    Outlined API

---
+++ dataset.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import os.path, os
+
+class Dataset:
+
+    def __init__(self, train_path, test_path, masks_path):
+        pass
+
+    def draw(self, size):
+        pass
